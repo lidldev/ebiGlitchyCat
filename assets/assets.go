7e710c2217a06740be9e3cd6bb43f6b76da95b49
Ahmet Affan <ahmet.affan.ebcioglu@outlook.com>
2023-12-30 19:28:07 +0300

    player added

---
+++ assets/assets.go
@@ -0,0 +1,29 @@
+package assets
+
+import (
+	"embed"
+	"image"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed *
+var assets embed.FS
+
+var MainSprite = getSingleImage("main.png")
+
+func getSingleImage(name string) *ebiten.Image {
+	f, err := assets.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+}
---
+++ game/player.go
@@ -0,0 +1,6 @@
+package game
+
+type Player struct {
+	x int
+	y int
+}
